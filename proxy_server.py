668eae4d116f9bba630a827781fe8d38598336c4
Ove <ove@5fourdigital.com>
2025-01-21 18:59:08 +0200

    Bind Flask app to 0.0.0.0 and dynamic PORT

--- proxy_server.py
+++ proxy_server.py
@@ -135,4 +135,5 @@ def serve_bundle_js():
     return "File not found", 404
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000,)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
